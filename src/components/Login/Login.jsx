eae4012e6cafa464e61721bcd62d8d76420e8e7d
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-04-17 03:18:05 +0600

    added google sign In & sign out option

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import {GoogleAuthProvider, getAuth} from "firebase/auth";
+import React, { useState } from 'react';
+import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from "firebase/auth";
 import app from '../../firbase/firbase.init';
 
 
 const Login = () => {
+    const [user , setUser] = useState(null);
     const auth = getAuth(app);
     const authProvider = new GoogleAuthProvider();
 
     const handelGoogleSignIn = () =>{
+        signInWithPopup(auth, authProvider)
+        .then(result =>{
+            const loggedInUser= result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
+        })
+        .catch(error =>{
+            console.log('Error: ', error.message);
+        })
+    }
 
-        console.log("Google mama is Coming !!!");
+    const handelSignOut = () =>{
+        signOut(auth)
+        .then(result =>{
+            console.log(result);
+            setUser(null);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+
     }
 
     return (
         <div>
+            {/* user ? logout : sign in */}
+
+            
+            {user ?
+            <button onClick={handelSignOut}>Sign Out</button> :
             <button onClick={handelGoogleSignIn}>Google login</button>
+            }
+            {user && <div>
+               <h1> User : {user?.displayName} </h1>
+               <p>Email : {user.email}</p>
+               <img src={user.photoURL} alt="" />
+            </div>
+            }
         </div>
     );
 };
